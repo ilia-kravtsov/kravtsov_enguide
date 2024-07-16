b1e3c87bd989ff46bc2d13a87fe4fb7beddab458
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-07-16 06:24:13 +0300

    Feature: Add formik

--- src/components/Dictionary.tsx
+++ src/components/Dictionary.tsx
@@ -11,24 +11,46 @@ export const Dictionary = ({}: Props) => {
 
     let [dataLoadFlag, setDataLoadFlag] = useState<boolean>(false)
     let [data, setData] = useState<Array<{ word: string, transcription: string, translation: string, comment: string}>>()
+    let [editCommentFlag, setEditCommentFlag] = useState<boolean>(false)
+    let [loadingWordsFlag, setLoadingWordsFlag] = useState<boolean>(false)
+    let [idsArray, setIdsArray] = useState<Array<any>>([])
 
+    // получаю данные с сервера
     useEffect( () => {
+        setLoadingWordsFlag(true)
+        // создал асинхронную функцию для получения данных
         const fetchData = async () => {
             const db = getDatabase(app); // getting database
             const dbReference = ref(db, 'words');
             const snapshot = await get(dbReference);
             if (snapshot.exists()) {
+
+                // получение id для редактирования комментария
+                const actualData = snapshot.val();
+                const idsArray = Object.keys(actualData).map(id => {
+                    return {
+                        ...actualData[id],
+                        id
+                    }
+                })
+                setIdsArray(idsArray)
                 setData(Object.values(snapshot.val()))
+                setLoadingWordsFlag(false)
             } else {
                 alert("data hasn't been downloaded")
             }
         };
-
         fetchData();
-        console.log('fetchData')
+
     }, [dataLoadFlag])
 
-    console.log(data);
+    const onCommentChangeClick = () => {
+        setEditCommentFlag(true)
+    }
+
+    const onSaveChangedComment = () => {
+        setEditCommentFlag(false)
+    }
 
     return (
         <div className={s.container}>
@@ -36,18 +58,30 @@ export const Dictionary = ({}: Props) => {
             <InputData setDataLoadFlag={setDataLoadFlag}/>
             <h2>Dictionary</h2>
             <ul>
-                {data?.map(((word, index) => {
-                    return (
-                        <li key={index} className={s.wordContainer}>
-                            <div className={s.wordMainInfo}>
-                                <div className={s.word}>{word.word}</div>
-                                <div className={s.wordComment}>{word.comment}</div>-
-                                <div>{word.transcription}</div>-
-                                <div>{word.translation.toLowerCase()}</div>
-                            </div>
-                        </li>
-                    )
-                }))}
+                {loadingWordsFlag
+                    ? <div className={s.loadingWords}>Loading...</div>
+                    : data?.map(((word, index) => {
+                        return (
+                            <li key={index} className={s.wordContainer}>
+                                <div className={s.wordMainInfo}>
+                                    <div className={s.word}>{word.word}</div>
+                                    {editCommentFlag
+                                        ? <div>
+                                            <textarea className={s.textareaCommentEdit}>{word.comment}</textarea>
+                                            <button className={s.buttonCommentEdit} onClick={onSaveChangedComment}>Save</button>
+                                        </div>
+                                        : <div className={s.wordComment} onClick={onCommentChangeClick}>
+                                            {word.comment}
+                                        </div>
+                                    }
+                                    -
+                                    <div>{word.transcription}</div>-
+                                    <div>{word.translation.toLowerCase()}</div>
+                                </div>
+                            </li>
+                        )
+                    }))
+                }
             </ul>
         </div>
     );
--- src/components/InputData.tsx
+++ src/components/InputData.tsx
@@ -3,6 +3,13 @@ import {ChangeEvent, useState} from "react";
 import {getDatabase, ref, set, push} from "firebase/database";
 // @ts-ignore
 import { app } from "../../firebase-config.ts";
+import {v4} from 'uuid';
+
+type ComplexityLevels = 'A1' | 'A2' | 'B1' | 'B2' | 'C1' | 'C2';
+
+type Data = {
+    id: string, word: string, transcription: string, translation: string, comment?: string, complexity: ComplexityLevels
+}
 
 type Props = {
     setDataLoadFlag: (dataLoadFlag: boolean) => void
@@ -10,22 +17,28 @@ type Props = {
 
 export const InputData = ({setDataLoadFlag}: Props) => {
 
-    let [data, setData] = useState<{word: string, transcription: string, translation: string, comment?: string}>({word: '', transcription: '', translation: '', comment: ''})
+    const complexityLevels: ComplexityLevels[]  =  ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
+
+    let [data, setData] = useState<Data>({id: '', word: '', transcription: '', translation: '', comment: '', complexity: 'A1'})
 
     const WordEntering = (e: ChangeEvent<HTMLInputElement>) => {
         setData({...data, word: e.currentTarget.value})
     }
-
     const TranscriptionEntering = (e: ChangeEvent<HTMLInputElement>) => {
         setData({...data, transcription: e.currentTarget.value})
     }
-
     const TranslationEntering = (e: ChangeEvent<HTMLInputElement>) => {
         setData({...data, translation: e.currentTarget.value})
     }
-
     const CommentEntering = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setData({...data, comment: e.currentTarget.value})
+    }
+
+    const handleCategoryChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value
+        if (complexityLevels.some(level => level === value)) {
+            setData({ ...data, complexity: value as ComplexityLevels });
+        }
     }
 
     const onDateSaveClick = async () => {
@@ -34,24 +47,28 @@ export const InputData = ({setDataLoadFlag}: Props) => {
         if (data.word.length > 0 && data.transcription.length > 0) {
 
             // готовлю данные
+            const id = v4();
             const word = data.word;
             const transcription = data.transcription;
             const translation = data.translation;
             const comment = data.comment;
+            const complexity = data.complexity
 
             // отправляю данные
             set(newWordReference, {
+                id,
                 word,
                 transcription,
                 translation,
-                comment
+                comment,
+                complexity
             }).then(() => {
                 alert('a new word saved successfully');
                 setDataLoadFlag(true)
             }).catch((error) => {
                 alert(`error: ${error}`);
             }).finally(() => {
-                setData({word: '', transcription: '', translation: '', comment: ''})
+                setData({id, word: '', transcription: '', translation: '', comment: '', complexity: 'A1'})
             })
         } else {
             alert('fill in all the fields');
@@ -59,18 +76,77 @@ export const InputData = ({setDataLoadFlag}: Props) => {
     }
 
     return (
-        <div className={s.container}>
+        <form className={s.container}>
             <div className={s.inputsContainer}>
                 <input className={s.input} onChange={WordEntering} value={data.word} maxLength={50}
                        placeholder={'new word'}/>
                 <input className={s.input} onChange={TranscriptionEntering} value={data.transcription} maxLength={50}
                        placeholder={'transcription'}/>
             </div>
-            <input className={s.input} onChange={TranslationEntering} value={data.translation} maxLength={50}
-                       placeholder={'translation'}/>
-            <textarea className={s.textarea} onChange={CommentEntering} placeholder={'comment'} rows={5} value={data.comment}></textarea>
-            <button className={s.saveButton} onClick={onDateSaveClick}>Save</button>
-        </div>
+            <input className={s.input} onChange={TranslationEntering} value={data.translation} maxLength={500}
+                   placeholder={'translation'}/>
+            <textarea className={s.textarea} onChange={CommentEntering} placeholder={'usage example'} rows={5}
+                      value={data.comment}/>
+            <h4>Specify the complexity of the word :</h4>
+            <div className={s.complexityContainer}>
+                <label className={s.complexityLabel}>
+                    <input
+                        type="radio"
+                        value="A1"
+                        name="complexity"
+                        checked={data.complexity === 'A1'}
+                        onChange={handleCategoryChange}
+                    />
+                    A1
+                </label>
+                <label className={s.complexityLabel}>
+                    <input
+                        type="radio"
+                        value="A2"
+                        name="complexity"
+                        onChange={handleCategoryChange}
+                    />
+                    A2
+                </label>
+                <label className={s.complexityLabel}>
+                    <input
+                        type="radio"
+                        value="B1"
+                        name="complexity"
+                        onChange={handleCategoryChange}
+                    />
+                    B1
+                </label>
+                <label className={s.complexityLabel}>
+                    <input
+                        type="radio"
+                        value="B2"
+                        name="complexity"
+                        onChange={handleCategoryChange}
+                    />
+                    B2
+                </label>
+                <label className={s.complexityLabel}>
+                    <input
+                        type="radio"
+                        value="C1"
+                        name="complexity"
+                        onChange={handleCategoryChange}
+                    />
+                    C1
+                </label>
+                <label className={s.complexityLabel}>
+                    <input
+                        type="radio"
+                        value="C2"
+                        name="complexity"
+                        onChange={handleCategoryChange}
+                    />
+                    C2
+                </label>
+            </div>
+            <button className={s.saveButton} onClick={onDateSaveClick} type={'button'}>Save</button>
+        </form>
     );
 };
 
